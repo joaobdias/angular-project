cbcd171c97a9efbf2dfa9d8c79583ac0804404d6
joaobdias <joaobdiasbr@gmail.com>
2026-03-06 12:11:34 -0300

    feat: finalizando footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @NgModule({
-  declarations: [AppComponent, CabecalhoComponent],
+  declarations: [AppComponent, CabecalhoComponent, RodapeComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
