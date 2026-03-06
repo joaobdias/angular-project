7c4bf5863efc4150dbdfb7254e3ac24caab1149e
joaobdias <joaobdiasbr@gmail.com>
2026-03-06 14:27:26 -0300

    feat: comunicando componentes com @Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component'
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarPensamentoComponent } from './componentes/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
+import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
 
 @NgModule({
-  declarations: [AppComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent, ListarPensamentosComponent],
+  declarations: [AppComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent, ListarPensamentosComponent, PensamentoComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamentos',
   templateUrl: './listar-pensamentos.component.html',
-  styleUrls: ['./listar-pensamentos.component.css']
+  styleUrls: ['./listar-pensamentos.component.css'],
 })
 export class ListarPensamentosComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'Comunicao entre componentes',
+      autoria: 'Juan',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Comunicao entre componentes 2',
+      autoria: 'Jorge',
+      modelo: 'modelo2',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
