46cd0274e86cdeb07300d7442ce9819942f37123
joaobdias <joaobdiasbr@gmail.com>
2026-03-06 14:41:24 -0300

    feat: template condicional com ngclass

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -7,16 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
   listaPensamentos = [
-    // {
-    //   conteudo: 'Comunicao entre componentes',
-    //   autoria: 'Juan',
-    //   modelo: 'modelo3',
-    // },
-    // {
-    //   conteudo: 'Comunicao entre componentes 2',
-    //   autoria: 'Jorge',
-    //   modelo: 'modelo2',
-    // },
+    {
+      conteudo: 'Comunicao entre componentes',
+      autoria: 'Juan',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Comunicao entre componentes 2',
+      autoria: 'Jorge',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo:
+        'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
+      autoria: '',
+      modelo: 'modelo1',
+    },
   ];
 
   constructor() {}
