68c544d88311e7926193ad638823bb19c020b187
joaobdias <joaobdiasbr@gmail.com>
2026-03-09 12:36:37 -0300

    feat: definindo interface para comunicacao com o backend

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamentos',
@@ -6,24 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamentos.component.css'],
 })
 export class ListarPensamentosComponent implements OnInit {
-  listaPensamentos = [
-    {
-      conteudo: 'Comunicao entre componentes',
-      autoria: 'Juan',
-      modelo: 'modelo3',
-    },
-    {
-      conteudo: 'Comunicao entre componentes 2',
-      autoria: 'Jorge',
-      modelo: 'modelo2',
-    },
-    {
-      conteudo:
-        'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
-      autoria: '',
-      modelo: 'modelo1',
-    },
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() {}
 
